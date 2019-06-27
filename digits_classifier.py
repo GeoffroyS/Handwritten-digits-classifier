5f65e8ca2d0acc5e7b0dea7f6d45648cb71af534
Geoffroy Sarrazin <sarrazin.geoffroy@gmail.com>
2019-06-27 02:05:32 +0100

    bloop

--- digits_classifier.py
+++ digits_classifier.py
@@ -1,8 +1,11 @@
 import os
-import numpy as np
 import struct
 
-def load_mnist (path, kind='train'):
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+def load_mnist (path, kind='train', validation_set=False):
 	# load ubyte files downloaded from Yann LeCun's page
 	# http://yann.lecun.com/exdb/mnist/
 	labels_path = os.path.join(path, "%s-labels-idx1-ubyte" % kind)
@@ -18,8 +21,11 @@ def load_mnist (path, kind='train'):
 		magic, num, rows, cols = struct.unpack('>IIII', pixels_h.read(16))
 		pixels = np.fromfile(pixels_h, dtype=np.uint8).reshape(len(labels), 784)
 
-	return pixels, labels
 
-pixels, labels = load_mnist('')
-print(labels.shape) # (60000,)
-print(pixels.shape) # (60000, 784): the images are squares of 28*28px = 784px
+	df = pd.DataFrame(pixels)
+	df['target'] = labels
+
+	return df
+
+print(load_mnist('').shape) # (60000, 785): 60k rows 
+							# and 28 by 28 = 784px images + 'target' column
