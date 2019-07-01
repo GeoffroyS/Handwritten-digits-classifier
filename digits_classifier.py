abe7aba03ae9eaf23cee6dc915b5ed5a363c843a
Geoffroy Sarrazin <sarrazin.geoffroy@gmail.com>
2019-07-01 22:25:51 +0100

    adding a couple of plots of the dataset

--- digits_classifier.py
+++ digits_classifier.py
@@ -48,10 +48,14 @@ def load_mnist (path, kind='train'):
 
 	return df_dict
 
-def _display_0_9(datasets_dict):
+def _display_digits(datasets_dict, plot_type='zero_nine', digit=3):
 	'''
-	take the first instances of the 10 digits from the training set 
-	and display them
+	- plot_type = 'zero_nine':
+	  take the first instances of the 10 digits
+	  from the training set and display them
+	- plot_type = 'same_digit':
+	  display the first 10 instances of a single digit
+	  ('digit' arg, 3 by default)
 	'''
 	fig, ax = plt.subplots(nrows=2, ncols=5, sharex=True, sharey=True)
 	ax = ax.flatten()
@@ -59,19 +63,33 @@ def _display_0_9(datasets_dict):
 	y_train = datasets_dict["train"].iloc[:,-1].to_numpy()
 	df_train = datasets_dict["train"]
 
-	for i in range(10):
-		img = X_train[y_train == i][0].reshape(28, 28)
-		ax[i].imshow(img, cmap='Greys', interpolation='nearest')
+	if plot_type == 'zero_nine':
+		for i in range(10):
+			img = X_train[y_train == i][0].reshape(28, 28)
+			ax[i].imshow(img, cmap='Greys', interpolation='nearest')
+	elif plot_type == 'same_digit':
+		for i in range(10):
+			img = X_train[y_train == digit][i].reshape(28, 28)
+			ax[i].imshow(img, cmap='Greys', interpolation='nearest')
+
 	ax[0].set_xticks([])
 	ax[0].set_yticks([])
 	plt.tight_layout()
 	plt.show()
+
+def _display_digits_distrib(datasets_dict):
+	df_train = datasets_dict["train"]
+	for i in range(10):
+		print('number of instances of {} in dataset: {}'.format(i, df_train.loc[df_train['target'] == i].shape[0]))
 
 if __name__ == '__main__':
 	datasets_dict = load_mnist('')
 	for dataset_type in datasets_dict:
 		print(dataset_type, datasets_dict[dataset_type].shape)
 
-	_display_0_9(datasets_dict)
+	_display_digits(datasets_dict)
+	_display_digits(datasets_dict, plot_type='same_digit', digit=8)
+	_display_digits_distrib(datasets_dict)
+
 
 
