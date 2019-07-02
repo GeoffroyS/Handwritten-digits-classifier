9264884c731dcad9a98b3957af9a9b4c15307bbd
Geoffroy Sarrazin <sarrazin.geoffroy@gmail.com>
2019-07-02 01:27:24 +0100

    neuralnet.py created

---
+++ neuralnet.py
@@ -0,0 +1,8 @@
+"""
+neuralnet.py
+
+A module implementing the stochastic gradient descent
+learning algorithm for a feedforward (Recurrent Neural Net coming up next!)
+neural net.
+Gradients calculated using backpropagation
+"""
